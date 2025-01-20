ab3f0316deba58ec9f4d41d21938f05c8bd16778
AnaLauraOliva <ana.loliva@estudiantes.matcom.uh.cu>
2025-01-20 07:55:58 -0500

    Ver se se solucionó el error con git

--- Witches Maze Runners/obj/Debug/net8.0/Witches Maze Runners.AssemblyInfo.cs
+++ Witches Maze Runners/obj/Debug/net8.0/Witches Maze Runners.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Witches Maze Runners")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c594ed8f92a4a03a9ad0cb2a620ab83cf5b73e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f26c6c045aa50d36dd7a0b091236611fd9174a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Witches Maze Runners")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Witches Maze Runners")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
